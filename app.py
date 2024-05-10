d96783a4689b19a1d28f6faf99fcdec7a800f76d
aaderinto04 <141211738+aaderinto04@users.noreply.github.com>
2024-05-10 13:07:35 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from keras.models import load_model
 from sklearn.preprocessing import MinMaxScaler
 import streamlit as st
 
-model = load_model('OneDrive\Documents\StockPriceProj\StockPredictionsModelKeras.zip')
+model = load_model('OneDrive\Documents\StockPriceProj\desktop.ini')
 
 st.header('Tesla Stock Predictor')
 
